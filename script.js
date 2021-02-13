1d9f3d95e40f1b4599ef8197994c4cdd1cc09122
jmolina518 <jmolina518@gmail.com>
2021-02-13 09:12:21 -0600

    add update function

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ next.addEventListener('click', () => {
   if (currentActive > circles.length) {
     currentActive = circles.length
   }
+  update()
 })
 
 prev.addEventListener('click', () => {
@@ -19,4 +20,15 @@ prev.addEventListener('click', () => {
   if (currentActive < 1) {
     currentActive = 1
   }
+  update()
 })
+
+function update() {
+  circles.forEach((circle, idx) => {
+    if (idx < currentActive) {
+      circle.classList.add('active')
+    } else {
+      circle.classList.remove('active')
+    }
+  })
+}
